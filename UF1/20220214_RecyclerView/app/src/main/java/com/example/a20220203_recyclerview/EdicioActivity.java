39244131a8182f6e34d1ecd894a2ef49129456b0
bernat <bernat.orellana@gmail.com>
2022-02-21 21:26:17 +0100

    afegir binding

--- UF1/20220214_RecyclerView/app/src/main/java/com/example/a20220203_recyclerview/EdicioActivity.java
+++ UF1/20220214_RecyclerView/app/src/main/java/com/example/a20220203_recyclerview/EdicioActivity.java
@@ -3,8 +3,10 @@ package com.example.a20220203_recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Debug;
 import android.provider.MediaStore;
@@ -15,7 +17,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.example.a20220203_recyclerview.databinding.ActivityEdicioBinding;
 import com.example.a20220203_recyclerview.model.Card;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class EdicioActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,11 +36,16 @@ public class EdicioActivity extends AppCompatActivity implements View.OnClickLis
     int indexCarta;
     //-----------------
     private Bitmap thumbnail;
-
+    private ActivityEdicioBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edicio);
+
+        binding = ActivityEdicioBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        //setContentView(R.layout.activity_edicio);
         //-------------------------------------
         // Sé que m'estan invocant des d'una altra activity
         // Prenem els paràmetres de dins de l'intent
@@ -76,20 +87,52 @@ public class EdicioActivity extends AppCompatActivity implements View.OnClickLis
         finish(); // tanca l'Activity i torna a l'anterior
     }
 
-
-
     //------------------------------------------------
     // Gestió de l'Intent de Càmera
     //------------------------------------------------
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
+
+    public static final int PICK_IMAGE = 1;
+
+    public void capturePhoto(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),
+                PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && data != null) {
+            try {
+                InputStream inputStream = this.getContentResolver().openInputStream(
+                        data.getData());
+                thumbnail = BitmapFactory.decodeStream(inputStream);
+                    imvFoto.setImageBitmap(thumbnail);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap..
+        }
+    }
+
+    /*
+
     public void capturePhoto() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         Log.d("Aplicacio", "Passo a dins????");
-        //if (intent.resolveActivity(getPackageManager()) != null) {
+        //---------------------------------------------------------------------
+        // Engeguem l'activity de càmera (si és que el mòbil la té, és clar! )
+        try {
             Log.d("Aplicacio", "SIIII Passo a dins");
             startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-        //}
+        } catch (ActivityNotFoundException ex) {
+            Log.e("Aplicacio","Activity no trobada", ex);
+        }
     }
 
     @Override
@@ -99,5 +142,5 @@ public class EdicioActivity extends AppCompatActivity implements View.OnClickLis
             thumbnail = data.getParcelableExtra("data");
             imvFoto.setImageBitmap(thumbnail);
         }
-    }
+    }*/
 }
